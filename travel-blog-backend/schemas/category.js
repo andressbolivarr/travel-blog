823426269a9f9e64a0fe66bdfb758cd37a103edd
Andres Bolivar <81452502+andressbolivarr@users.noreply.github.com>
2022-05-23 20:48:26 -0400

    add: schemas

--- travel-blog-backend/schemas/category.js
+++ travel-blog-backend/schemas/category.js
@@ -1,17 +1,17 @@
-export default{
+export default {
     name: "category",
     type: "document",
     title: "Category",
     fields: [
         {
-        name: "title",
-        type: "string",
-        title: "Title"
-    },
-    {
-        name: "description",
-        type: "text",
-        title: "Description"
-    }
-]
+            name: "title",
+            type: "string",
+            title: "Title"
+        },
+        {
+            name: "description",
+            type: "text",
+            title: "Description"
+        }
+    ]
 }
